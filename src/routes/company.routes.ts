daca10d0132073a623e90f8889541b320f9343e3
Marcus Nilsson <marcusnilsson.webb@gmail.com>
2019-08-12 15:49:54 +0200

    Sets up the remove a user from company route.

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -29,6 +29,11 @@ function setUpCompanyRoutes(app){
     companyController.updateCompany(req, res);
   });
 
+  // Remove a user from a company
+  app.post('/companies/:companyId/remove-user', (req: express.Request, res: express.Response) => {
+    companyController.removeUserFromCompany(req, res);
+  });
+
   // Delete a company with companyId
   app.delete('/companies/:companyId', (req: express.Request, res: express.Response) => {
     companyController.deleteCompany(req, res);
