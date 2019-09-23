f954f3552381ff41172ab50268d003a77a91e5ea
Marcus Nilsson <marcusnilsson.webb@gmail.com>
2019-09-23 10:25:30 +0200

    Adds empty seed migration file.

---
+++ src/migration/1569226833849-SeedRoles.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+import Role from '../entities/role.entity'
+export class SeedRoles1569226833849 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
