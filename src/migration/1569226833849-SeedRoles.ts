1085d521614bbeb1aaa015a60f2776923723169d
Marcus Nilsson <marcusnilsson.webb@gmail.com>
2019-09-23 10:55:57 +0200

    Adds the migration file for seeding initial roles.

--- src/migration/1569226833849-SeedRoles.ts
+++ src/migration/1569226833849-SeedRoles.ts
@@ -1,11 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, getRepository} from "typeorm";
 import Role from '../entities/role.entity'
+
+// Seed file
+const ROLES = require('../seeds/roles.json');
+
 export class SeedRoles1569226833849 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+
+        let theRoles = [];
+        ROLES.forEach(role => {
+            const formattedRole = new Role();
+            formattedRole.role = role.role;
+            theRoles.push(formattedRole);
+        });
+
+        await getRepository(Role).save(theRoles);
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+
+        let listOfRoles = [];
+        const getRolesFromSeed = async () => await Promise.all(ROLES.map(async role => {
+            const fetchedRole = await getRepository(Role).findOne({ role: role.role }); 
+            listOfRoles.push(fetchedRole);
+        }))
+
+        await getRolesFromSeed();
+        await getRepository(Role).remove(listOfRoles);
+
     }
 
 }
