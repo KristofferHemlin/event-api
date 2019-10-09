4a019c82fbbccfa5ceb17636a6a1f46d0874572a
Marcus Nilsson <marcusnilsson.webb@gmail.com>
2019-10-09 13:30:17 +0200

    Changes the ormconfig to use .env variables.

---
+++ ormconfig.js
@@ -0,0 +1,22 @@
+module.exports = {
+   "type": "postgres",
+   "host": process.env.DB_HOST,
+   "port": process.env.DB_PORT,
+   "username": process.env.DB_USERNAME,
+   "password": process.env.DB_PASSWORD,
+   "database": process.env.DB_DATABASE,
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      "src/entities/**/*.ts"
+   ],
+   "migrations": [
+      "src/migration/**/*.ts"
+   ],
+   "cli": {
+      "migrationsDir": "src/migration"
+   },
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ]
+}
