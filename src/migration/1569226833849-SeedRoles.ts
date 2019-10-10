5244cde094533a3adf122eb197df5e816d794949
CLAMON-8244LRQ\Kristoffer Hemlin <kristoffer.hemlin@claremont.se>
2019-10-10 10:10:47 +0200

    Comment out CSV

--- src/migration/1569226833849-SeedRoles.ts
+++ src/migration/1569226833849-SeedRoles.ts
@@ -1,35 +1,35 @@
-import {MigrationInterface, QueryRunner, getRepository} from "typeorm";
-import Role from '../entities/role.entity'
+// import {MigrationInterface, QueryRunner, getRepository} from "typeorm";
+// import Role from '../entities/role.entity'
 
-// Seed file
-const ROLES = require('../seeds/roles.json');
+// // Seed file
+// const ROLES = require('../seeds/roles.json');
 
-export class SeedRoles1569226833849 implements MigrationInterface {
+// export class SeedRoles1569226833849 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
+//     public async up(queryRunner: QueryRunner): Promise<any> {
 
-        let theRoles = [];
-        ROLES.forEach(role => {
-            const formattedRole = new Role();
-            formattedRole.role = role.role;
-            theRoles.push(formattedRole);
-        });
+//         let theRoles = [];
+//         ROLES.forEach(role => {
+//             const formattedRole = new Role();
+//             formattedRole.role = role.role;
+//             theRoles.push(formattedRole);
+//         });
 
-        await getRepository(Role).save(theRoles);
+//         await getRepository(Role).save(theRoles);
 
-    }
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
+//     public async down(queryRunner: QueryRunner): Promise<any> {
 
-        let listOfRoles = [];
-        const getRolesFromSeed = async () => await Promise.all(ROLES.map(async role => {
-            const fetchedRole = await getRepository(Role).findOne({ role: role.role }); 
-            listOfRoles.push(fetchedRole);
-        }))
+//         let listOfRoles = [];
+//         const getRolesFromSeed = async () => await Promise.all(ROLES.map(async role => {
+//             const fetchedRole = await getRepository(Role).findOne({ role: role.role }); 
+//             listOfRoles.push(fetchedRole);
+//         }))
 
-        await getRolesFromSeed();
-        await getRepository(Role).remove(listOfRoles);
+//         await getRolesFromSeed();
+//         await getRepository(Role).remove(listOfRoles);
 
-    }
+//     }
 
-}
+// }
