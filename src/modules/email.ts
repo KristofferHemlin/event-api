0d11cc24f8b50bb090b8473f245a37e74b4d65e1
Johanna Bromark <johanna.bromark@gmail.com>
2019-10-15 09:36:56 +0200

    Change to gmail

--- src/modules/email.ts
+++ src/modules/email.ts
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 export const transporter = nodemailer.createTransport({
-    service: "Hotmail",
+    service: "Gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
