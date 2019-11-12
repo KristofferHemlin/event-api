982d38dfec04010d5ca8e7a7ac57e9fc0c16f4a1
Johanna Bromark <johanna.bromark@gmail.com>
2019-11-12 12:58:20 +0100

    Uncomment middleware for event routes

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -6,9 +6,9 @@ function setUpEventRoutes(app) {
 
 
   // Middleware
-  // app.use('/events', (req, res, next) => {
-  //   isAuthenticated(req, res, next);
-  // })
+  app.use('/events', (req, res, next) => {
+    isAuthenticated(req, res, next);
+  })
 
   /**
    * @api {get} Fetch event by its id
