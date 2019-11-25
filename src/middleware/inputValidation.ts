b9a74e7d36ba86e080a40eeed8229a1b0bd311dc
Johanna Bromark <johanna.bromark@gmail.com>
2019-11-25 08:04:11 +0100

    Remove console log

--- src/middleware/inputValidation.ts
+++ src/middleware/inputValidation.ts
@@ -16,7 +16,6 @@ export function cleanAndValidateEvent(req, res, next) {
 function cleanAndValidate(req, res, next, validationFn) {
     const input = cleanInput(req.body);
     const [inputValid, errorMessage, errorDetails] = validationFn(input);
-    console.log("input valid:", inputValid);
     if (!inputValid) {
         removeFile(req.file);
         res.status(400).send({message: errorMessage, details: errorDetails, status: 400});
